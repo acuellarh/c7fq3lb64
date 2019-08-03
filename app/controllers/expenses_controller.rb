c1129e554829f714230410c035b10553f8baaedb
Alejandro <acuellarh@gmail.com>
2019-08-02 19:41:08 -0500

    Feature to show just the expenses of the loged user

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,6 +4,7 @@ class ExpensesController < ApplicationController
 
   def index  
     @expenses = Expense.order("date DESC")
+    @expenses = Expense.where(user_id: current_user.id)
       if params[:concept].present?
         @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")       
       end
